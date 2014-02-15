1366e35248ab7e896a4d46ee0147c465461639e3
Shawn Webb <lattera@gmail.com>
2014-02-15 11:43:52 -0500

    Add more functions

--- VirtualMachine.go
+++ VirtualMachine.go
@@ -15,6 +15,7 @@ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
 package VirtualMachine
 
 type VirtualMachine interface {
+    GetUUID() string
     Start() error
     Stop() error
     Status() string
